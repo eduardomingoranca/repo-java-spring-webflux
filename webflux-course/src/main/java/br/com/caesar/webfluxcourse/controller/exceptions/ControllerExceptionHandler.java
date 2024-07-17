1983e02a7a21c3a62861686f38c5a2a48d83113f
eduardomingoranca <eduardomingoranca@gmail.com>
2024-07-17 16:09:18 -0300

    add exception handler validation error

--- webflux-course/src/main/java/br/com/caesar/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ webflux-course/src/main/java/br/com/caesar/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -4,13 +4,16 @@ package br.com.caesar.webfluxcourse.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.ResponseEntity.badRequest;
+import static org.springframework.http.ResponseEntity.status;
 import static reactor.core.publisher.Mono.just;
 
 @ControllerAdvice
@@ -29,6 +32,21 @@ public class ControllerExceptionHandler {
                         .path(request.getPath().toString())
                         .build()
         ));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(
+            WebExchangeBindException ex, ServerHttpRequest request
+    ) {
+        ValidationError error = new ValidationError(
+                now(), request.getPath().toString(),
+                BAD_REQUEST.value(), "Validation error", "Error on validation attributes"
+        );
+
+        for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+        return status(BAD_REQUEST).body(just(error));
     }
 
     private String verifyDupKey(String message) {
--- webflux-course/src/main/java/br/com/caesar/webfluxcourse/model/request/UserRequest.java
+++ webflux-course/src/main/java/br/com/caesar/webfluxcourse/model/request/UserRequest.java
@@ -1,8 +1,20 @@
 package br.com.caesar.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 // record => eh uma funcionalidade do java 14 para a criacao de classes de valor mais concisas.
 public record UserRequest(
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
+        @NotBlank(message = "must not be null or empty")
         String name,
+
+        @Email(message = "invalid email")
+        @NotBlank(message = "must not be null or empty")
         String email,
+
+        @NotBlank(message = "must not be null or empty")
+        @Size(min = 3, max = 20, message = "must be between 3 and 20 characters")
         String password
 ) { }
